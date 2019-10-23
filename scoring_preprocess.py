7060af6b96549e731a946a6b1fc0027f66631863
Rohit Gupta <rohitgupta.hpf@gmail.com>
2019-10-23 18:12:25 -0400

    Scoring Preprocess doesn't need to map colors to class ... since saved file is an indexed PNG

--- scoring_preprocess.py
+++ scoring_preprocess.py
@@ -7,7 +7,7 @@ from PIL import Image
 import numpy as np
 
 
-EPOCH = 60
+EPOCH = 69
 
 prefix = "samples/200/"
 pre_pred_path = prefix + "pre_pred_epoch_" + str(EPOCH)
@@ -20,12 +20,16 @@ post_gt = open_image_as_nparray(post_gt_path + ".png", dtype=np.int64)
 pre_pred = open_image_as_nparray(pre_pred_path + ".png", dtype=np.int64)
 post_pred = open_image_as_nparray(post_pred_path + ".png", dtype=np.int64)
 
-pre_gt_class = convert_color_segmap_to_int(pre_gt)
-pre_gt_class[pre_gt_class > 1] = 1
-post_gt_class = convert_color_segmap_to_int(post_gt)
-pre_pred_class = convert_color_segmap_to_int(pre_pred)
-pre_pred_class[pre_pred_class > 1] = 1
-post_pred_class = convert_color_segmap_to_int(post_pred)
+# pre_gt_class = convert_color_segmap_to_int(pre_gt)
+# pre_gt_class[pre_gt_class > 1] = 1
+# post_gt_class = convert_color_segmap_to_int(post_gt)
+# pre_pred_class = convert_color_segmap_to_int(pre_pred)
+# pre_pred_class[pre_pred_class > 1] = 1
+# post_pred_class = convert_color_segmap_to_int(post_pred)
+
+pre_gt[pre_gt > 1] = 1
+pre_pred[pre_pred > 1] = 1
+
 
 # for i in range(H):
 #     for j in range(W):
@@ -39,16 +43,16 @@ post_pred_class = convert_color_segmap_to_int(post_pred)
 pathlib.Path("scoring/targets/").mkdir(parents=True, exist_ok=True)
 pathlib.Path("scoring/predictions/").mkdir(parents=True, exist_ok=True)
 
-print(np.max(pre_gt_class)), print(np.min(pre_gt_class))
-print(np.max(post_gt_class)), print(np.min(post_gt_class))
+print(np.max(pre_gt)), print(np.min(pre_gt))
+print(np.max(post_gt)), print(np.min(post_gt))
 
-print(np.max(pre_pred_class)), print(np.min(pre_pred_class))
-print(np.max(post_pred_class)), print(np.min(post_pred_class))
+print(np.max(pre_pred)), print(np.min(pre_pred))
+print(np.max(post_pred)), print(np.min(post_pred))
 
-Image.fromarray(pre_gt_class).save("scoring/targets/test_localization_00000_target.png")
-Image.fromarray(post_gt_class).save("scoring/targets/test_damage_00000_target.png")
-Image.fromarray(pre_pred_class).save("scoring/predictions/test_localization_00000_prediction.png")
-Image.fromarray(post_pred_class).save("scoring/predictions/test_damage_00000_prediction.png")
+Image.fromarray(pre_gt).save("scoring/targets/test_localization_00000_target.png")
+Image.fromarray(post_gt).save("scoring/targets/test_damage_00000_target.png")
+Image.fromarray(pre_pred).save("scoring/predictions/test_localization_00000_prediction.png")
+Image.fromarray(post_pred).save("scoring/predictions/test_damage_00000_prediction.png")
 
 
 
