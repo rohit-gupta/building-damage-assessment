f95b0c3a0c7f9ef33fec7d309d734d4132c98931
Rohit Gupta <rohitgupta.hpf@gmail.com>
2019-10-23 21:16:19 -0400

    Apply to all samples

--- scoring_preprocess.py
+++ scoring_preprocess.py
@@ -5,6 +5,7 @@
 import pathlib
 from PIL import Image
 import numpy as np
+from glob import glob
 
 colors = [[  0,   0, 200], # Blue:   Background
           [  0, 200,   0], # Green:  No Damage
@@ -19,16 +20,52 @@ def open_image_as_nparray(img_path, dtype):
 
 EPOCH = 69
 
-prefix = "samples/200/"
-pre_pred_path = prefix + "pre_pred_epoch_" + str(EPOCH)
-post_pred_path = prefix + "post_pred_epoch_" + str(EPOCH)
-pre_gt_path = prefix + "pre_gt"
-post_gt_path = prefix + "post_gt"
+prefix = "samples/"
 
-pre_gt = open_image_as_nparray(pre_gt_path + ".png", dtype=np.uint8)
-post_gt = open_image_as_nparray(post_gt_path + ".png", dtype=np.uint8)
-pre_pred = open_image_as_nparray(pre_pred_path + ".png", dtype=np.uint8)
-post_pred = open_image_as_nparray(post_pred_path + ".png", dtype=np.uint8)
+
+samples = glob(prefix + "*/")
+
+
+for idx, sample in enumerate(samples):
+    pre_pred_path = sample + "pre_pred_epoch_" + str(EPOCH)
+    post_pred_path = sample + "post_pred_epoch_" + str(EPOCH)
+    pre_gt_path = sample + "pre_gt"
+    post_gt_path = sample + "post_gt"
+
+    pre_gt = open_image_as_nparray(pre_gt_path + ".png", dtype=np.uint8)
+    post_gt = open_image_as_nparray(post_gt_path + ".png", dtype=np.uint8)
+    pre_pred = open_image_as_nparray(pre_pred_path + ".png", dtype=np.uint8)
+    post_pred = open_image_as_nparray(post_pred_path + ".png", dtype=np.uint8)
+
+    pre_gt[pre_gt > 1] = 1
+    pre_pred[pre_pred > 1] = 1
+    post_pred = post_pred*pre_pred
+
+    pathlib.Path("scoring/targets/").mkdir(parents=True, exist_ok=True)
+    pathlib.Path("scoring/predictions/").mkdir(parents=True, exist_ok=True)
+    pathlib.Path("scoring/predictions_color/").mkdir(parents=True, exist_ok=True)
+
+    r = Image.fromarray(pre_pred)
+    r.putpalette(colors)
+    r.save("scoring/predictions_color/" +str(idx)+ "localization.png")
+    r = Image.fromarray(post_pred)
+    r.putpalette(colors)
+    r.save("scoring/predictions_color/" +str(idx)+ "classification.png")
+
+    id = str(idx)
+    id = "".join(["0"]*(5 - len(id) + id))
+
+    Image.fromarray(pre_gt).save("scoring/targets/test_localization_" +id+ "_target.png")
+    Image.fromarray(post_gt).save("scoring/targets/test_damage_" +id+ "_target.png")
+    Image.fromarray(pre_pred).save("scoring/predictions/test_localization_" +id+ "_prediction.png")
+    Image.fromarray(post_pred).save("scoring/predictions/test_damage_" +id+ "_prediction.png")
+
+
+# print(np.max(pre_gt)), print(np.min(pre_gt))
+# print(np.max(post_gt)), print(np.min(post_gt))
+#
+# print(np.max(pre_pred)), print(np.min(pre_pred))
+# print(np.max(post_pred)), print(np.min(post_pred))
 
 # pre_gt_class = convert_color_segmap_to_int(pre_gt)
 # pre_gt_class[pre_gt_class > 1] = 1
@@ -37,9 +74,6 @@ post_pred = open_image_as_nparray(post_pred_path + ".png", dtype=np.uint8)
 # pre_pred_class[pre_pred_class > 1] = 1
 # post_pred_class = convert_color_segmap_to_int(post_pred)
 
-pre_gt[pre_gt > 1] = 1
-pre_pred[pre_pred > 1] = 1
-post_pred = post_pred*pre_pred
 
 
 # for i in range(H):
@@ -51,26 +85,6 @@ post_pred = post_pred*pre_pred
 # Hack, we know all colors have unique R+G values
 
 
-pathlib.Path("scoring/targets/").mkdir(parents=True, exist_ok=True)
-pathlib.Path("scoring/predictions/").mkdir(parents=True, exist_ok=True)
-
-print(np.max(pre_gt)), print(np.min(pre_gt))
-print(np.max(post_gt)), print(np.min(post_gt))
-
-print(np.max(pre_pred)), print(np.min(pre_pred))
-print(np.max(post_pred)), print(np.min(post_pred))
-
-r = Image.fromarray(pre_pred)
-r.putpalette(colors)
-r.save("scoring/predictions/localization.png")
-r = Image.fromarray(post_pred)
-r.putpalette(colors)
-r.save("scoring/predictions/classification.png")
-
-Image.fromarray(pre_gt).save("scoring/targets/test_localization_00000_target.png")
-Image.fromarray(post_gt).save("scoring/targets/test_damage_00000_target.png")
-Image.fromarray(pre_pred).save("scoring/predictions/test_localization_00000_prediction.png")
-Image.fromarray(post_pred).save("scoring/predictions/test_damage_00000_prediction.png")
 
 
 
