3f3df28fa035ccf686d0eab27c773d899f7bb82b
Rohit Gupta <rohitgupta.hpf@gmail.com>
2019-10-29 19:41:24 -0400

    Add val results

--- results_json_to_csv.py
+++ results_json_to_csv.py
@@ -1,11 +1,14 @@
 import json
 
 
+#PREFIX = "scores/sample_score_e"
+PREFIX = "val_scores/epoch_"
+
 print("score", "damage_f1", "localization_f1",
       "damage_f1_no_damage", "damage_f1_minor_damage",
       "damage_f1_major_damage", "damage_f1_destroyed")
 for i in range(200):
-    r = json.load(open("scores/sample_score_e" + str(i) + ".json", "r"))
+    r = json.load(open(PREFIX + str(i) + ".json", "r"))
     print(r["score"], r["damage_f1"],
           r["localization_f1"],
           r["damage_f1_no_damage"],
