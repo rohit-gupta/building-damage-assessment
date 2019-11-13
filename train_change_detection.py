5b8ee0b7d8d26db4ec0188bca13bd99f9b0bbed7
Rohit Gupta <rohitgupta.hpf@gmail.com>
2019-11-13 13:50:48 -0500

    Debugging model code

--- train_change_detection.py
+++ train_change_detection.py
@@ -3,9 +3,11 @@ import torch
 from change_detection_model import ChangeDetectionNet
 
 
+
 changenet = ChangeDetectionNet(classes=5, num_layers=2, feature_channels=15,
                                kernel_scales=[3, 13, 23], dilation_scales=[2, 4, 8],
                                use_bn=True, padding_type="replication")
+cuda = torch.device('cuda')
 changenet = changenet.cuda()
 x = torch.rand(8,10,1024,1024)
 x = x.cuda()
