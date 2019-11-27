4a3162c47aba17f411d887e9a5e8cfa3be1d82b1
Rohit Gupta <rohitgupta.hpf@gmail.com>
2019-11-26 22:41:46 -0500

    minor fixes to test script

--- test.py
+++ test.py
@@ -29,7 +29,7 @@ config_file = config_name + ".ini"
 config = configparser.ConfigParser()
 config.read(config_file)
 
-BEST_EPOCH = 98
+BEST_EPOCH = config["paths"]["BEST_MODEL"]
 
 if torch.cuda.is_available():
     device = "cuda"
