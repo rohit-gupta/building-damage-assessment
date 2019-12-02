76e82e039bc0d537c60b8be20b8b576e470b714c
Rohit Gupta <rohitgupta.hpf@gmail.com>
2019-12-01 21:01:32 -0500

    Further progress on change detection

--- train_change_detection.py
+++ train_change_detection.py
@@ -15,7 +15,7 @@ from utils import clean_distributed_state_dict
 from utils import reconstruct_from_tiles
 from functools import partial
 from losses import cross_entropy, localization_aware_cross_entropy
-from train_utils import save_model
+from train_utils import save_model, save_val_results, save_val_gt
 
 config_name = os.environ["XVIEW_CONFIG"]
 config = read_config(config_name, config_type="change")
@@ -77,35 +77,32 @@ CROP_END = config["dataloader"]["CROP_SIZE"] - CROP_BEGIN
 BATCH_SIZE = config["dataloader"]["BATCH_SIZE"]
 NUM_TILES = config["dataloader"]["CROP_SIZE"] // config["dataloader"]["TILE_SIZE"]
 NUM_TILES *= NUM_TILES
+TILE_SIZE = config["dataloader"]["CROP_SIZE"]/NUM_TILES
 semseg_model.eval()
 
 
 MODELS_FOLDER = config["paths"]["MODELS"] + config_name + "/"
 pathlib.Path(MODELS_FOLDER).mkdir(parents=True, exist_ok=True)
 
-
 for epoch in range(int(config["hyperparameters"]["NUM_EPOCHS"])):
+
+    changenet.train()
     for idx, (pretiles, posttiles, prelabels, postlabels) in enumerate(trainloader):
         with torch.set_grad_enabled(False):
             segs = []
             labels = []
             for i in range(BATCH_SIZE):
-                pretiles[i] = pretiles[i].to(gpu1)
-                posttiles[i] = posttiles[i].to(gpu1)
+                pretiles[i] = pretiles[i].to(gpu0)
+                posttiles[i] = posttiles[i].to(gpu0)
                 segmentations = semseg_model(torch.cat((pretiles[i], posttiles[i])))
                 segmentations = segmentations.cpu()
 
                 pre_seg = [reconstruct_full(segmentations[:NUM_TILES, :, :, :])]
                 post_seg = [reconstruct_full(segmentations[NUM_TILES:, :, :, :])]
                 segs += [torch.cat((pre_seg, post_seg), dim=0)]
-
-                # pre_labels = reconstruct_full(prelabels[i].cpu())
-                # post_labels = reconstruct_full(postlabels[i].cpu())
-                # labels += [torch.cat((pre_labels[0, :, :], post_labels[1:, :, :]), dim=0)]
                 labels = reconstruct_full(postlabels[i].cpu())
 
             input_batch = torch.stack(segs, dim=0)
-
             # crop off labels for edges
             labels_batch = torch.stack(labels[:, CROP_BEGIN:CROP_END, CROP_BEGIN:CROP_END], dim=0)
 
@@ -138,9 +135,25 @@ for epoch in range(int(config["hyperparameters"]["NUM_EPOCHS"])):
 
             optimizer.step()
 
-        #TODO save val samples
-        if epoch % config["misc"]["SAVE_FREQ"] == 0:
-            save_model(changenet.state_dict(), MODELS_FOLDER, epoch)
+        changenet.eval()
+        for idx, (pretiles, posttiles, prelabels, postlabels) in enumerate(valloader):
+            # TODO compute results for val set
+            # TODO
+            # TODO
+            # TODO
+            # TODO
+            # TODO
+            # TODO save val results for epoch
+            # Write to disk for visually tracking training progress
+            save_path = "val_results/" + config_name + "/" + str(idx) + "/"
+            pathlib.Path(save_path).mkdir(parents=True, exist_ok=True)
+            save_val_results(save_path, epoch, config["hyperparameters"]["LOSS"], cropped_preds, cropped_preds, tiled=False)
+            # Save groundtruth
+            if epoch == 0:  # Groundtruth only needs to be saved once
+                save_val_gt(save_path,  TILE_SIZE)
+            # Save model
+            if epoch % config["misc"]["SAVE_FREQ"] == 0:
+                save_model(changenet.state_dict(), pretiles[0], posttiles[0], prelabels[0], postlabels[0], MODELS_FOLDER, epoch)
 
 
 
