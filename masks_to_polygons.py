246e8b3a7334aee342c638dc2ef07a808f6b15ea
Rohit Gupta <rohitgupta.hpf@gmail.com>
2019-12-08 17:33:09 -0500

    Basic script to convert masks to polygons

--- masks_to_polygons.py
+++ masks_to_polygons.py
@@ -1,8 +1,21 @@
 
 
+import numpy as np
+from imantics import Polygons, Mask
+from PIL import Image
+
+def open_image_as_nparray(img_path, dtype):
+    return np.array(Image.open(img_path), dtype=dtype)
+
 
 location = "/home/c3-0/rohitg/xview_results/val_results/turing_finetune_tier1/"
-preds = "epoch10_val_results"
-gt = "gt_val_results"
+preds = "epoch10_val_results/pred_"
+gt = "gt_val_results/post_"
+
 count = 304
-selected = 70
+selected = "70.png"
+pred = open_image_as_nparray(location + preds + selected, dtype=np.uint8)
+pred[pred > 1] = 1
+polygons = Mask(pred).polygons()
+print(polygons.points)
+print(polygons.segmentation)
