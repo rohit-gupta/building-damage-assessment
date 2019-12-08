c5837d767261cb8860b93dbde9634be3bf97c7f4
Rohit Gupta <rohitgupta.hpf@gmail.com>
2019-12-08 17:50:47 -0500

    Developing mask-to-polygons script

--- masks_to_polygons.py
+++ masks_to_polygons.py
@@ -3,6 +3,7 @@
 import numpy as np
 from imantics import Polygons, Mask
 from PIL import Image
+from shapely.geometry import Polygon
 
 def open_image_as_nparray(img_path, dtype):
     return np.array(Image.open(img_path), dtype=dtype)
@@ -17,13 +18,21 @@ selected = "70.png"
 pred = open_image_as_nparray(location + preds + selected, dtype=np.uint8)
 pred[pred > 1] = 1
 polygons = Mask(pred).polygons()
-print(len(polygons.points))
+# print(len(polygons.points))
 # print(polygons.segmentation)
 
 
+shp_poly = Polygon(polygons.points)
+shp_poly_simp = shp_poly.simplify(0.05, preserve_topology=True)
 for x in polygons.points:
     print(len(x))
+i_poly_simpy = Polygons(shp_poly_simp.exterior.coords.xy)
 
 img = np.zeros((1024,1024,3)).astype(np.uint8)
 polygons.draw(img)
 Image.fromarray(img).save("masks_to_polygons/" + selected)
+
+
+img = np.zeros((1024,1024,3)).astype(np.uint8)
+i_poly_simpy.draw(img)
+Image.fromarray(img).save("masks_to_polygons/simplified_" + selected)
