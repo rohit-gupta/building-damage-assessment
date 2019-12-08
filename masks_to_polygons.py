31c05ee829510a59022228d716fc9d8cadf03210
Rohit Gupta <rohitgupta.hpf@gmail.com>
2019-12-08 18:03:14 -0500

    Developing mask-to-polygons script

--- masks_to_polygons.py
+++ masks_to_polygons.py
@@ -9,6 +9,16 @@ def open_image_as_nparray(img_path, dtype):
     return np.array(Image.open(img_path), dtype=dtype)
 
 
+def arr_to_int(x):
+    return [int(x) for i in x]
+
+
+def shapely_poly_to_imantics_poly(shp_poly):
+    x = arr_to_int(shp_poly[0][1])
+    y = arr_to_int(shp_poly[0][1])
+
+    return [[x[i], y[i]]for i in range(len(x))]
+
 location = "/home/c3-0/rohitg/xview_results/val_results/turing_finetune_tier1/"
 preds = "epoch10_val_results/pred_"
 gt = "gt_val_results/post_"
@@ -28,7 +38,7 @@ for x in polygons.points:
         continue
     shp_poly = Polygon(x)
     shp_poly_simp = shp_poly.simplify(0.05, preserve_topology=True)
-    simp_polys.append(shp_poly_simp.exterior.coords.xy)
+    simp_polys.append(shapely_poly_to_imantics_poly(shp_poly_simp.exterior.coords.xy))
     print(x, shp_poly_simp.exterior.coords.xy)
 i_poly_simp = Polygons(simp_polys)
 
