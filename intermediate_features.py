51efb2783ec6040009fae051cfc2284fdb417752
Rohit Gupta <rohitgupta.hpf@gmail.com>
2019-12-09 20:16:00 -0500

    Developing code to get intermediate features

--- intermediate_features.py
+++ intermediate_features.py
@@ -6,6 +6,8 @@ import torch
 from torchvision.models.segmentation import deeplabv3_resnet50
 from torchvision.utils import save_image
 
+from torch.nn.functional import interpolate
+
 from config_parser import read_config
 from utils import clean_distributed_state_dict
 
@@ -24,5 +26,6 @@ semseg_model.load_state_dict(clean_distributed_state_dict(torch.load(config["cha
 
 layer1_weights = semseg_model.backbone.conv1.weight
 print(layer1_weights.shape)
+layer1_weights = interpolate(layer1_weights, scale_factor=20.0, mode='nearest')
 
-save_image(layer1_weights, "layer1_weights.png")
+save_image(layer1_weights, "layer1_weights_upsampled.png")
