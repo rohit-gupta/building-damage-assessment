047aa2e07936450fc58ae2fcff5db99f0c7eeede
Rohit Gupta <rohitgupta.hpf@gmail.com>
2019-12-10 18:42:57 -0500

    further developing intermediate features model;results for focal finetuning on tier1

--- intermediate_features.py
+++ intermediate_features.py
@@ -85,23 +85,81 @@ _, valloader, _ = xview_train_loader_factory("segmentation",
                                           False,
                                           1024,
                                           1024,  # No tiling
-                                          4,
-                                          1,
+                                          config["dataloader"]["BATCH_SIZE"],
+                                          config["dataloader"]["THREADS"],
                                           False)
 
 
 MODELS_FOLDER = config["paths"]["MODELS"] + config_name + "/"
 pathlib.Path(MODELS_FOLDER).mkdir(parents=True, exist_ok=True)
 
+optimizer = optim.AdamW(changenet.parameters(),
+                        lr=config["hyperparams"]["INITIAL_LR"],
+                        weight_decay=config["hyperparams"]["WEIGHT_DECAY"])
+
+
+if config["misc"]["APEX_OPT_LEVEL"] != "None":
+    changenet, optimizer = amp.initialize(changenet, optimizer, opt_level=config["misc"]["APEX_OPT_LEVEL"])
+
+
+# Logging
+train_loss = AverageMeter("train_loss")
+train_loss_log = MetricLog("train_loss")
+
 for epoch in range(int(config["hyperparams"]["NUM_EPOCHS"])):
     train_pbar = tqdm.tqdm(total=len(trainloader))
     changenet = changenet.train()
 
     for idx, (pretiles, posttiles, prelabels, postlabels) in enumerate(trainloader):
-        pretiles_batch = torch.cat(pretiles, dim=0)
-        posttiles_batch = torch.cat(posttiles, dim=0)
+        pretiles_batch = torch.cat(pretiles, dim=0).to(gpu1)
+        posttiles_batch = torch.cat(posttiles, dim=0).to(gpu1)
         prelabels_batch = torch.cat(prelabels, dim=0)
         postlabels_batch = torch.cat(postlabels, dim=0)
+        pre_seg = semseg_model(pretiles_batch)['out']
+        post_seg = semseg_model(posttiles_batch)['out']
+        # TODO Logits to probs for batch
+        pre_features = layer1_features(pretiles_batch)
+        post_features = layer1_features(posttiles_batch)
+        change_input = torch.cat((pre_seg, post_seg, pre_features, post_features), dim=1)
+        optimizer.zero_grad()
+        with torch.set_grad_enabled(True):
+            preds = changenet(change_input).cpu()
+
+            if config["hyperparams"]["LOSS"] == "crossentropy":
+                loss = cross_entropy(postlabels_batch, preds)
+            elif config["hyperparams"]["LOSS"] == "locaware":
+                loc_loss, cls_loss, loss = localization_aware_loss(postlabels_batch, preds,
+                                                                config["hyperparams"]["LOC_WEIGHT"],
+                                                                config["hyperparams"]["CLS_WEIGHT"])
+            
+            if config["misc"]["APEX_OPT_LEVEL"] != "None":
+                with amp.scale_loss(loss, optimizer) as scaled_loss:
+                    scaled_loss.backward()
+            else:
+                loss.backward()
+            
+            optimizer.step()
+            
+        loss_val = loss.item()
+        train_loss.update(val=loss_val)
+
+        train_pbar.update(1)
+    
+    train_pbar.close()
+    train_loss_log.update(train_loss.value())
+    
+    changenet = changenet.eval()
+
+    for idx, (pretiles, posttiles, prelabels, postlabels) in enumerate(valloader):
+        pretiles_batch = torch.cat(pretiles, dim=0).to(gpu1)
+        posttiles_batch = torch.cat(posttiles, dim=0).to(gpu1)
+        segmentations = semseg_model(torch.cat((pretiles_batch, posttiles_batch), dim=0))['out'].cpu()
+        # TODO Complete this
+
+
+
+
+
 
 
 
