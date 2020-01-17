7dc7d1b4a0f72e6a40b5bb85f158bb750143a6d4
Rohit Gupta <rohitgupta.hpf@gmail.com>
2020-01-17 17:01:01 -0500

    re-organize unnecessary files

--- run_val_scoring.sh
+++
@@ -1,6 +0,0 @@
-N=$((XVIEW_EPOCHS))
-for i in $(seq 0 1 $N)
-do
- python xview2_metrics.py val_scoring/$XVIEW_CONFIG/$i/predictions/ val_scoring/$XVIEW_CONFIG/targets/ val_scores/$XVIEW_CONFIG/epoch_$i.json
-done
-
